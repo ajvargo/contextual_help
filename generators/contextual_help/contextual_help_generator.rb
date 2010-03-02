ca87707fa2d017640fafa26213330fa0fc9bb18c
Andrew J Vargo <ajvargo@computer.org>
2010-03-01 22:55:11 -0500

    Add creation of view folders

--- generators/contextual_help/contextual_help_generator.rb
+++ generators/contextual_help/contextual_help_generator.rb
@@ -3,9 +3,16 @@ class ContextualHelpGenerator < Rails::Generator::NamedBase
     record do |m|
       unless options[:no_migration]
         migration = "create_contextual_help_tables"
-        m.migration_template "#{migration}.rb", 'db/migrate',
+        m.migration_template "db/#{migration}.rb", 'db/migrate',
         :assigns => {:migration_name => migration },
         :migration_file_name => migration
+      end
+  
+      unless options[:no_views]
+        m.directory "app/views/help_articles"
+        m.directory "app/views/help_locations"
+        m.directory "app/views/shared"
+        
       end
       # m.directory "lib"
       # m.template 'README', "README"
