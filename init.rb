cc001b1759590a92d990fd8c00b6dfa8809e77c5
Andrew J Vargo <ajvargo@computer.org>
2010-03-07 16:18:03 -0500

    Added controller to plugin.

--- init.rb
+++ init.rb
@@ -1,21 +1,21 @@
-# Include hook code here
+ActionController::Base.send(:include, ContextualHelp)
+#ActionController::Base.helper(ContextualHelp)
 
 
-puts <<-eos
+# puts <<-eos
 
 
-  To complete setup, you'll need to run:
-    rake db:migrate
+#   To complete setup, you'll need to run:
+#     rake db:migrate
 
-  Also, add the following to config/routes. These will hook-up Contextual Help for you.
+#   Also, add the following to config/routes. These will hook-up Contextual Help for you.
   
-  # Standard REST for the Articles and location
-  map.resources :help_articles
-  map.resources :help_locations
+#   # Standard REST for the Articles and location
+#   map.resources :help_articles
+#   map.resources :help_locations
 
-  # These allow anything/help to get picked up on by Contextual Help     
-  map.connect ':controller/help', :action => :help
-  map.connect ':controller/:crud/help', :action => :help
+#   # These allow anything/help to get picked up on by Contextual Help     
+#   map.connect ':controller/help', :action => :help
+#   map.connect ':controller/:rest/help', :action => :help
 
-
-eos
+# eos
