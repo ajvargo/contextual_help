c1be0f00d65a2adb90e5c7f11e5c53c83615f5e6
Andrew J Vargo <ajvargo@computer.org>
2010-03-14 22:54:14 -0400

    Added options to skip parts of generation

--- generators/contextual_help/contextual_help_generator.rb
+++ generators/contextual_help/contextual_help_generator.rb
@@ -1,15 +1,14 @@
 class ContextualHelpGenerator < Rails::Generator::NamedBase
-
   def manifest
     record do |m|
-      unless options[:no_migration]
+      unless options[:skip_migration]
         migration = "create_contextual_help_tables"
         m.migration_template "db/#{migration}.rb", 'db/migrate',
         :assigns => {:migration_name => migration },
         :migration_file_name => migration
       end
   
-      unless options[:no_views]
+      unless options[:skip_views]
         m.directory "app/views/help_locations"
         %W[index edit new show].each do |view|
           m.file  "app/views/help_locations/#{view}.html.erb", "app/views/help_locations/#{view}.html.erb"
@@ -24,10 +23,21 @@ class ContextualHelpGenerator < Rails::Generator::NamedBase
         m.file  "app/views/shared/help.html.erb", "app/views/shared/help.html.erb"
       end
 
-      unless options[:no_routes]
+      unless options[:skip_routes]
         m.route_resources "help_articles"
         m.route_resources "help_locations"
       end
     end
+  end
+  
+  def add_options!(opt)
+    opt.separator ''
+    opt.separator 'Options:'
+    opt.on("--skip-migration",
+           "Don't generate a migration file for help locations or articles") { |v| options[:skip_migration] = true }
+    opt.on("--skip-views",
+      "Don't generate a copy of the views") { |v| options[:skip_views] = true }
+    opt.on("--skip-routes",
+      "Don't add in contextual help releated routes") { |v| options[:skip_routes] = true }
   end
 end
