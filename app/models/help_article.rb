177b219f8db5120860a08831eb52b1938e0c7a2a
Andrew J Vargo <ajvargo@computer.org>
2010-03-22 22:28:16 -0400

    Created in_use named scope

--- app/models/help_article.rb
+++ app/models/help_article.rb
@@ -1,8 +1,7 @@
 class HelpArticle < ActiveRecord::Base
   has_many :help_article_locations  
   has_many :help_locations, :through => :help_article_locations 
-  
-  def self.help_article_count(controller, action)
-    HelpLocation.find_all_by_action_and_controller("index", "comments").inject(0){|c,hl| c += hl.help_articles.count}
-  end
+
+  named_scope :in_use, lambda{|controller, action| { :include => :help_locations, :conditions => ['help_locations.controller = ? AND help_locations.action = ? AND help_articles.in_use = ?', controller, action, true] }}
+
 end
