f3bf4492255270df5f01a5c7069ceea1701d6fc3
Andrew J Vargo <ajvargo@computer.org>
2010-03-22 22:29:23 -0400

    Added options to contextual_link page_tip always_link name id class

--- lib/contextual_help_helper.rb
+++ lib/contextual_help_helper.rb
@@ -1,11 +1,18 @@
 module ContextualHelpHelper
-  def contextual_help_link
-    unless (
-            !['help_article','help_location'].include? controller_name &&
-            action_name != "help" &&
-            HelpArticle.help_article_count(controller_name, action_name) < 1
-            )
-      link_to 'Help', request.request_uri.chomp("/") + "/help"
+  def contextual_help_link(options = {})
+    options[:always_link] ||= false 
+    options[:name] ||= 'Help'
+    options[:page_tip] ||= false
+
+    if show_link? options[:always_link]
+      link_to options[:name], request.request_uri.chomp("/") + "/help" + (options[:page_tip] ? "&page_tip=1" : ''), :id => options[:id], :class => options[:class]
     end
+  end
+
+  private
+  def show_link?(always_link)
+    return false if ['help_article','help_location'].include? controller_name || action_name == 'help'
+    return true if always_link
+    return HelpArticle.in_use(controller_name, action_name).count > 0
   end
 end
